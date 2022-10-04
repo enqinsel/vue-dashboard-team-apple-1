eb6a0e046e743a3b6510bc2dcb507e268efca0ac
kaplanark <kaplanarkan@gmail.com>
2022-10-05 00:53:07 +0300

    added Input.vue Select.vue Multiselect.vue Textarea.vue for Form.vue and added Offcanvas.vue

--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -1,37 +1,52 @@
 <script setup>
-import { ref } from 'vue';
-import Search from '@components/Inputs/Search.vue';
-import UserInfo from '@components/User/UserInfo.vue';
-import Button from '@components/Button/Button.vue';
-import Badge from '@components/Badge/Badge.vue';
-
-const props = defineProps(['title']);
-const data = ref({
-	image: 'https://avatars.githubusercontent.com/u/2681643',
-	name: 'Tolga Eğilmezel',
-	email: 'egilmezel@tolga.com'
-})
-
-const searchHandler = (value) => {
-	alert(value);
-}
-</script>
+	import { ref } from 'vue';
+	import Search from '@components/Inputs/Search.vue';
+	import UserInfo from '@components/User/UserInfo.vue';
+	import Button from '@components/Button/Button.vue';
+	import Badge from '@components/Badge/Badge.vue';
+	import Offcanvas from '@components/Offcanvas/Offcanvas.vue';
 	
-<template>
-	<header class="header">
-		<div class="header__title">
-			{{ title }}
-		</div>
-		<div class="header__content">
-			<Search placeholder="Search" @search="searchHandler" />
-		</div>
-		<div class="header__actions">
-			<div class="btn-group">
-				<Button icon="chat" variant="icon" classes="text-strong" />
-				<Badge icon="notifications" active />
-				<Button icon="settings" variant="icon" classes="text-strong" />
-			</div>
-			<UserInfo :data="data" />
-		</div>
-	</header>
-</template>
+	const props = defineProps(['title']);
+	const data = ref({
+		 image: 'https://avatars.githubusercontent.com/u/2681643',
+		 name: 'Tolga Eğilmezel',
+		 email: 'egilmezel@tolga.com'
+	})
+	
+	const toggleNotification = ref(false);
+	
+	const toggleHandler = () => {
+		 toggleNotification.value = !toggleNotification.value;
+	};
+	
+	const searchHandler = (value) => {
+		 alert(value);
+	}
+	</script>
+	
+	<template>
+		 <header class="header">
+			  <div class="header__title">
+					{{ title }}
+			  </div>
+			  <div class="header__content">
+					<Search placeholder="Search" @search="searchHandler" />
+			  </div>
+			  <div class="header__actions">
+					<div class="btn-group">
+						 <Button icon="chat" variant="icon" classes="text-strong" />
+						 <Badge icon="notifications" active @click="toggleHandler" />
+						 <Button icon="settings" variant="icon" classes="text-strong" />
+					</div>
+					<UserInfo :data="data" />
+			  </div>
+			  <Offcanvas v-model:show="toggleNotification">
+					<template #header>
+						 <h3 class="text-secondary">Notification</h3>
+					</template>
+					<template #body>
+						
+					</template>
+			  </Offcanvas>
+		 </header>
+	</template>
